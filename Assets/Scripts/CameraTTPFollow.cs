929855938e41b23e06bcec8da6ad50f7bab5be26
ManAgainstDragon <managainstdragon@gmail.com>
2013-11-29 09:01:08 -0800

    Camera following bullet

---
+++ Assets/Scripts/CameraTTPFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+
+public class CameraTTPFollow : MonoBehaviour {
+    
+    private Transform _target;
+    private Transform _camera;
+
+    public float _disZ = 4.0f;
+    public float _disY = 1.5f;
+
+	void Start () {
+        _target = transform;
+        _camera = Camera.main.transform;
+	}
+	
+	void Update () {
+        _camera.position = _target.position + new Vector3(0, _disY, -_disZ);
+        _camera.LookAt(_target);
+	}
+}
